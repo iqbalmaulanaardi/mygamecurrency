2e7a28ebf164009d2c28ed757a4b1ea43a1f5f50
Iqbal Maulana <iqbal@kawancicil.co.id>
2020-11-14 14:39:02 +0700

    delete responses

--- responses/transaction.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-type TransactionListResponse struct {
-	SourceUser string  `json:"source_user"`
-	DestUser   string  `json:"dest_user"`
-	Amount     float64 `json:"amount"`
-	Type       string  `json:"type"`
-	CreatedAt  string  `json:"created_at"`
-}
